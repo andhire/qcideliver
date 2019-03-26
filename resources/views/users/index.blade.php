3a7bdd1feca795bab1adc62cccbbff8632b1fa6b
poenix111 <nazazuke@gmail.com>
2019-03-26 11:51:50 -0600

    Identacion index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,72 +1,73 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Usuarios</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-        
-    </head>
-    <body>
+  <title>Usuarios</title>
 
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 
-        <nav class="navbar navbar-dark bg-dark ">
-                <div class="container text-center">
-                        <a class="navbar-brand" href="{{ url('/user')}}">Usuarios</a>
-                        <a class="navbar-brand" href="{{ url('/user/create')}}">Crear Usuario</a>
-                </div>
-        </nav>
-        
-        
-        
+</head>
 
-        
-        <div class="card-columns text-center">
-        @foreach ($users as $user)
-            
-
-        <a href="{{ url('/user',[$user->slug]) }}">
-        <div class="card text-center" style="width: 18rem;" action="/user/">
-                    <img class="card-img-top" src="{{$user->foto}}" height="268",width="180px" alt="Card image cap">
-                    <div class="card-body">
-                      <h5 class="card-title">{{$user->nombre}} {{$user->apellidoP}} {{$user->apellidoM}}</h5>
-
-                      
-
-                      
-                      <div class="row">
-                            <div class="col col-lg-5">
-                                    @if($user->estado==1)
-                                    <img src="{{asset('img/online.png')}}" height="16px",width="16px"/>
-                                      @else
-                                      <img src="{{asset('img/offline.png')}}" height="16px",width="16px"/>
-                                      @endif
-                             </div>
-                            <div class="col-md-auto">
-                                    @if($user->tipo==1)
-                                    <p class="card-text">Vendedor</p>
-                                  @else
-                                    <p class="card-text">Comprador</p>
-                                  @endif
-                            </div>
-                           
-                      </div>
+<body>
 
 
-                    </div>
-                  </div>
-        </a>
-        @endforeach
-
-
-        
+  <nav class="navbar navbar-dark bg-dark ">
+    <div class="container text-center">
+      <a class="navbar-brand" href="{{ url('/user')}}">Usuarios</a>
+      <a class="navbar-brand" href="{{ url('/user/create')}}">Crear Usuario</a>
     </div>
+  </nav>
+
+
+
+
+
+  <div class="card-columns text-center">
+    @foreach ($users as $user)
+
+
+    <a href="{{ url('/user',[$user->slug]) }}">
+      <div class="card text-center" style="width: 18rem;" action="/user/">
+        <img class="card-img-top" src="{{$user->foto}}" height="268" ,width="180px" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">{{$user->nombre}} {{$user->apellidoP}} {{$user->apellidoM}}</h5>
+
+
+
+
+          <div class="row">
+            <div class="col col-lg-5">
+              @if($user->estado==1)
+              <img src="{{asset('img/online.png')}}" height="16px" ,width="16px" /> @else
+              <img src="{{asset('img/offline.png')}}" height="16px" ,width="16px" /> @endif
+            </div>
+            <div class="col-md-auto">
+              @if($user->tipo==1)
+              <p class="card-text">Vendedor</p>
+              @else
+              <p class="card-text">Comprador</p>
+              @endif
+            </div>
+
+          </div>
+
+
         </div>
-        <footer class="pagination justify-content-center "style="margin-top:15%">
-            {{ $users->links() }}
-        </footer>
-        
-    </body>
+      </div>
+    </a>
+    @endforeach
+
+
+
+  </div>
+  </div>
+  <footer class="pagination justify-content-center " style="margin-top:15%">
+    {{ $users->links() }}
+  </footer>
+
+</body>
+
 </html>
