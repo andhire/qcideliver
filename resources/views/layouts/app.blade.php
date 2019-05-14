1ba07d454b60e06f30ef51997fd2b53eddf1d9ba
Edmundo Gonzalez <mundo2599@outlook.com>
2019-05-13 19:45:41 -0500

    agregado archivo borrado por error xd

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <!-- Google Font -->
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,700" rel="stylesheet">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
+
+  <!-- Bootstrap CSS -->
+  <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+
+  <!-- Custom CSS -->
+  <link href="{{asset('css/create.css')}}" rel="stylesheet">
+</head>
+
+<body>
+  <div id="app">
+    <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+      <div class="container">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <!-- Left Side Of Navbar -->
+          <ul class="navbar-nav mr-auto">
+
+          </ul>
+
+          <!-- Right Side Of Navbar -->
+          <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+
+            @if (Route::has('register'))
+            <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+              <a class="navbar-brand" href="/">
+                <img src="{{asset('img/icon.svg')}}" width="30" height="30" class="d-inline-block align-top"
+                  alt="QciDeliver">
+                QciDeliver
+              </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navLinks"
+                aria-controls="navLinks" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+
+              <div class="collapse navbar-collapse" id="navLinks">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="/">Inicio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="/product">Productos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a href="/login">
+                      <button type="button" class="btn btn-link">Inciar Sesión</button>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+
+            @endif
+            @else
+
+
+            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false" v-pre>
+              {{ Auth::user()->name }} <span class="caret"></span>
+            </a>
+
+
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+              {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+            </form>
+        </div>
+        </li>
+        @endguest
+        </ul>
+      </div>
+  </div>
+  </nav>
+
+  <main class="py-4">
+    @yield('content')
+  </main>
+  </div>
+</body>
+
+</html>
