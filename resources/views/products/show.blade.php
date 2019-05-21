14a8e054b518930bb132e92644a94aefafe58384
Edmundo Gonzalez <mundo2599@outlook.com>
2019-05-21 00:42:05 -0500

    mejorado show de producto y de usuario

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,52 +2,46 @@
 @section('title', ($product->name))
 @section('content')
 
-<div class="container mt-5" style="top: 10%">
+<div class="album py-5 bg-light">
+  <div class="container">
 
-  <div class="row">
+    <div class="card mb-4 shadow-sm">
 
-
-    <div class="container mt-5" style="top: 10%">
-
-      <div class="row">
-        <div class="col">
-
-          <div class="card" style="width: 18rem;">
-
-            <img src="{{$product->image}}" class="img-thumbnail" />
-          </div>
-        </div>
-        <div class="col">
-          <div class="row">
-            <h2>{{$product->name}}</h2>
-          </div>
-          <div class="row">
+      <img src="{{$product->image}}" width="100%" height="200">
+      <div class="card-body">
+        <h2 class="card-title">{{$product->name}}</h2>
+        <p class="card-text">
+          <div>
             <h5>Precio: ${{$product->price}}</h5>
           </div>
-          <div class="row">
+          <div>
             <h5>Cantidad: {{$product->amount}}</h5>
           </div>
-          <div class="row">
+          <div>
             <h5>Vendedor: {{$product->user->name}}</h5>
           </div>
-          <div class="row">
+          <div>
             <h5>Ubicacion: {{$product->user->userUbication->ubication->nombre ?? 'No hay ubicacion'}}</h5>
           </div>
-          <div class="row">
+          <div>
             <h5>Telefono: {{$product->user->phone}}</h5>
           </div>
-        </div>
+        </p>
 
         @if(Auth::check() && Auth::user()['tipo'] == 0)
-        <div style="margin: 5px;">
-          <a href="{{ url('/product/'.$product->slug.'/edit') }}"><button type="button"
-              class="btn btn-warning">Editar</button>
-        </div>
-        <div style="margin: 5px;">
-          <form action="{{ route('product.destroy', $product->slug) }}" method="POST">
-            @csrf @method('DELETE')
-            <button type="submit" class="btn btn-danger">Borrar</button>
-          </form>
+        <div class="d-flex justify-content-between align-items-center">
+          <div class="btn-group">
+            <div style="margin: 5px;">
+              <a href="{{ url('/product/'.$product->slug.'/edit') }}"><button type="button"
+                  class="btn btn-warning">Editar</button>
+            </div>
+            <div style="margin: 5px;">
+              <form action="{{ route('product.destroy', $product->slug) }}" method="POST">
+                @csrf @method('DELETE')
+                <button type="submit" class="btn btn-danger">Borrar</button>
+              </form>
+            </div>
+          </div>
         </div>
         @endif
 
@@ -56,8 +50,6 @@
     </div>
   </div>
 
-
-</div>
 </div>
 
 @endsection
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -2,82 +2,74 @@
 @section('title', '{{$user->nombre}}')
 @section('content')
 
-<div class="container mt-5">
 
-  <div class="row">
-    <div class="col">
+<div class="album py-5 bg-light">
+  <div class="container">
 
-      <div class="card" style="width: 18rem;">
+    <div class="card text-center mb-4 shadow-sm">
 
-        <img src="{{$user->foto}}" class="img-thumbnail" />
-      </div>
-    </div>
-    <div class="col">
-      <div class="row">
-        <h2>{{$user->nombre}} {{$user->apellidoP}} {{$user->apellidoM}}</h2>
+      <img src="{{$user->foto}}" width="100%" height="200">
 
-        <div class="col col-lg-5">
+      <div class="card-body">
+        <h5 class="card-title">
+          {{$user->name}} {{$user->apellidoP}} {{$user->apellidoM}}
+        </h5>
+        <p class="card-text">
+          @if($user->tipo==0)
+          Admin
+          @elseif($user->tipo==1)
+          Vendedor
+          @else
+          Comprador
+          @endif
           @if($user->estado==1)
           <img src="{{asset('img/online.png')}}" height="16px" ,width="16px" />
           @else
           <img src="{{asset('img/offline.png')}}" height="16px" ,width="16px" />
           @endif
-        </div>
+        </p>
 
-        <div class="col-md-auto">
-          @if($user->tipo==0)
-          <p class="card-text">Admin</p>
-          @elseif($user->tipo==1)
-          <p class="card-text">Vendedor</p>
-          @else
-          <p class="card-text">Comprador</p>
-          @endif
-        </div>
+        <p>
+          <div class="btn-group">
+            <a href="{{ url('/user/'.$user->slug.'/edit') }}"><button type="button"
+                class="btn btn-warning">Editar</button></a>
 
-        <div style="margin: 5px;">
-          <a href="{{ url('/user/'.$user->slug.'/edit') }}"><button type="button"
-              class="btn btn-warning">Editar</button>
-        </div>
+            <form action="{{ route('user.destroy', $user->slug) }}" method="POST">
+              @csrf @method('DELETE')
+              <button type="submit" class="btn btn-danger">Borrar</button>
+            </form>
+          </div>
+        </p>
 
-        <div style="margin: 5px;">
-          <form action="{{ route('user.destroy', $user->slug) }}" method="POST">
-            @csrf @method('DELETE')
-            <button type="submit" class="btn btn-danger">Borrar</button>
-          </form>
-        </div>
-
-        @if($user->tipo == 0)
-        <div style="margin: 5px;">
+        <div class="btn-group">
+          @if($user->tipo == 0)
           <form action="{{ url('/user/'.$user->id.'/quitar-admin')}}" method="POST">
             @csrf @method('POST')
-            <button type="submit" class="btn btn-principal">Quitar admin</button>
+            <button type="submit" class="btn btn-secondary">Quitar admin</button>
           </form>
-        </div>
-        @else
-        <div style="margin: 5px;">
+          @else
           <form action="{{ url('/user/'.$user->id.'/hacer-admin')}}" method="POST">
             @csrf @method('POST')
-            <button type="submit" class="btn btn-principal">Hacer admin</button>
+            <button type="submit" class="btn btn-secondary">Hacer admin</button>
           </form>
-        </div>
-        @endif
+          @endif
 
-        @if($user->estado == 0)
-        <div style="margin: 5px;">
+          @if($user->estado == 0)
           <form action="{{ url('/user/'.$user->id.'/aprobar')}}" method="POST">
             @csrf @method('POST')
             <button type="submit" class="btn btn-primary">Aprobar</button>
           </form>
-        </div>
-        @else
-        <div style="margin: 5px;">
+          @else
           <form action="{{ url('/user/'.$user->id.'/bloquear')}}" method="POST">
             @csrf @method('POST')
             <button type="submit" class="btn btn-danger">Bloquear</button>
           </form>
+          @endif
         </div>
-        @endif
+
       </div>
     </div>
+  </div>
+</div>
 
 @endsection
